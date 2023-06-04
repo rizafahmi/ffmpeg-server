bdc10427009519e54a2b853119b1b4fd0efc8d89
Riza Fahmi <rizafahmi@gmail.com>
2023-06-04 21:51:05 +0700

    Add styling with open-props

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'));
+app.use(express.static('node_modules/open-props'))
+
 
 const upload = multer({ storage: multer.memoryStorage() });
 
